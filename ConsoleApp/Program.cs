6c864fdd0d34d2b8be3784c8a6617ac8644265e3
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-03 12:11:55 +0200

    Nullable

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp;
+using System;
 
 //namespace ConsoleApp
 //{
@@ -8,6 +9,36 @@
 //        {
             Console.WriteLine("Hello, World!");
             void Sth() { }
+
+
+int? a = 5;
+a = null;
+
+Person? p = new Person();
+p.Name = "Ewa";
+
+ChangeInt(a);
+
+p = null;
+
+if(p != null)
+    ChangePerson(p);
+
+Console.WriteLine(a);
+Console.WriteLine(p.Name);
+
+
+void ChangeInt(int? a)
+{
+    a = 6;
+}
+
+void ChangePerson(Person p)
+{
+    p.Name = "Adam";
+}
+
+
 //        }
 //   }
 //}
