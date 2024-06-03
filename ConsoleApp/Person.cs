60e80a550034a12314b6f614e2ef040d776d9ad8
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-03 15:10:06 +0200

    Budowa klasy

--- ConsoleApp/Person.cs
+++ ConsoleApp/Person.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿//namespace - przestrzeń nazw, czyli adres pod którym "mieszka" klasa
+//namespace zaciągamy używając "using"
 namespace ConsoleApp
 {
+    //pełna nazwa klasy to <namespace>.<nazwaKlasy>
+
+    //class - szablon opisujący zachowania i cechy obiektów (instancji klas), które są wytwarzane na jej podstawie
+    //internal - modyfikator dostępu - oznacza, że z klasy można korzystać tylko w projekcie "rodzimym"
+    //public - modyfikator dostępu - oznacza, że z klasy można korzystać wszędzie
+    //brak modyfikatora dostępu = internal (najbardziej restrykcyjny modyfikator dostępu)
+
+    // <modyfikator dostępu> class <nazwaKlasy>
     internal class Person
     {
-        public string Name { get; set; }    
     }
 }
